97e736ed606c78c54fd1e9b1f4f7680e9ee857d4
ae.whitemamba <ae.whitemamba@github.com>
2022-12-10 12:41:00 +0900

    파라미터 코드올리기

---
+++ main.js
@@ -0,0 +1,19 @@
+function opend(구멍){
+  document.getElementById('alert').style.display = 구멍;
+}
+
+// function idAlarm(){
+//   document.getElementById('title').innerHTML = '아이디를 입력하세요';
+//   document.getElementById('alert').style.display = 'block';
+// }
+
+// function pwAlarm(){
+//   document.getElementById('title').innerHTML = '비밀번호를 입력하세요';
+//   document.getElementById('alert').style.display = 'block';
+// }
+
+function Alarm(구멍){
+  document.getElementById('title').innerHTML = 구멍;
+  document.getElementById('alert').style.display = 'block';
+}
+
